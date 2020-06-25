73c8815e14fb084d2e9a545909c97fc6cf8dfe2a
Dimuthu <kasunwpdimuthu@gmail.com>
2020-06-25 23:17:59 +0530

    app js added to src folder

---
+++ src/App.js
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import LoginScreen from 'src/screens/login'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <LoginScreen />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
