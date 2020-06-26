52ac858284ad72dfb0bba8129c23eded182f3983
Dimuthu <kasunwpdimuthu@gmail.com>
2020-06-26 18:53:14 +0530

    app rename to main

--- index.js
+++ index.js
@@ -2,8 +2,22 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import * as React from 'react';
+import { AppRegistry } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { Provider as StoreProvider } from 'react-redux';
 import App from './src/App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import store from './store';
 
-AppRegistry.registerComponent(appName, () => App);
+export default function Main() {
+  return (
+    <StoreProvider store={store}>
+      <PaperProvider>
+        <App />
+      </PaperProvider>
+    </StoreProvider>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => Main);
