b4f6e96a0f940b53a2a0b2fad8da53a5dd88b6a9
Dimuthu <kasunwpdimuthu@gmail.com>
2020-06-27 10:06:38 +0530

    multidex enabled in android

--- android/app/src/main/java/lk/ucsc/chatapp/MainApplication.java
+++ android/app/src/main/java/lk/ucsc/chatapp/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
