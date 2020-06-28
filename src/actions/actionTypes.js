1397fa42dc487fec4362b29c3b133fd6416e17af
Dimuthu <kasunwpdimuthu@gmail.com>
2020-06-28 15:38:54 +0530

    actions changed

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -1,3 +1,3 @@
 export const LOGIN_REQUEST = 'LOGIN_REQUEST';
-export const LOGIN_RESPONSE = 'LOGIN_RESPONSE';
 export const LOGIN_FAILED = 'LOGIN_FAILED';
+export const LOGIN_SUCCESS ='LOGIN_SUCCESS';
