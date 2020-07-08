c121585234f0b7a6efa0a8da854c7c278c7e1dbf
Dimuthu <kasunwpdimuthu@gmail.com>
2020-07-08 19:08:56 +0530

    camera and showimage screens added

--- src/screens/index.js
+++ src/screens/index.js
@@ -3,3 +3,5 @@ export { default as LoginScreen } from './LoginScreen';
 export { default as RegisterScreen } from './RegisterScreen';
 export { default as ForgotPasswordScreen } from './ForgotPasswordScreen';
 export { default as Dashboard } from './Dashboard';
+export { default as Camera } from './Camera';
+export { default as ShowImage } from './ShowImage';
