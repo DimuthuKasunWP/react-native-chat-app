7d00e746cc4b5aafbb187438682380642e914fd6
Dimuthu <kasunwpdimuthu@gmail.com>
2020-07-10 21:25:35 +0530

    fireabase configuration added

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+import 'firebase/storage'; 
+
+firebase.initializeApp({
+
+    apiKey: "AIzaSyDhXPG-FaIO2ZrNtQfqaW7pDMOJ4r3LrtY",
+    authDomain: "chat-app-71bd1.firebaseapp.com",
+    databaseURL: "https://chat-app-71bd1.firebaseio.com",
+    projectId: "chat-app-71bd1",
+    storageBucket: "chat-app-71bd1.appspot.com",
+    messagingSenderId: "594581396208",
+    appId: "1:594581396208:web:b39454a5aab5f71be8c035",
+    measurementId: "G-Z8H0RG77ZS"
+  
+});
+ export const  storageRef = firebase.storage().ref();
