de4ae3339d6c1d530f8cb8ded9cc77ec07f8fad9
Dimuthu <kasunwpdimuthu@gmail.com>
2020-07-11 10:06:34 +0530

    camera action types added

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -1,3 +1,7 @@
 export const LOGIN_REQUEST = 'LOGIN_REQUEST';
 export const LOGIN_FAILED = 'LOGIN_FAILED';
 export const LOGIN_SUCCESS ='LOGIN_SUCCESS';
+export const ADD_IMAGE ='ADD_IMAGE';
+export const SET_SPINNER ='SET_SPINNER';
+export const SET_SPINNER_TO_FALSE ='SET_SPINNER_TO_FALSE';
+export const GET_IMAGE ='GET_IMAGE';
