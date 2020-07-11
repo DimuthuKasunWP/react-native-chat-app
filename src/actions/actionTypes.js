b9ea2648c2d415fbf1c866d9a9ff6b1300e9cd06
Dimuthu <kasunwpdimuthu@gmail.com>
2020-07-11 19:07:48 +0530

    new action types added

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -5,3 +5,4 @@ export const ADD_IMAGE ='ADD_IMAGE';
 export const SET_SPINNER ='SET_SPINNER';
 export const SET_SPINNER_TO_FALSE ='SET_SPINNER_TO_FALSE';
 export const GET_IMAGE ='GET_IMAGE';
+export const UPLOAD_SUCCESS ='UPLOAD_SUCCESS'
