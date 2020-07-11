cfcb670e1b02df107f9551e78a14af01399e7361
Dimuthu <kasunwpdimuthu@gmail.com>
2020-07-11 10:07:52 +0530

    image reducer added

--- src/reducers/imageReducer.js
+++ src/reducers/imageReducer.js
@@ -1,37 +1,57 @@
+import createReducer from './createReducer';
+import * as actionTypes from 'src/actions/actionTypes';
+
+
+
 const initialState = {
   imageCollection: [],
   spinner: false,
-  imageIsAdded: false
+  imageIsAdded: false,
+  data:'', 
+  uri:''
 
 }
+ export const imageReducer = createReducer(initialState, {
+    [actionTypes.ADD_IMAGE](state,action) {
+        return {
+            ...state,
+            imageIsAdded: true,
+            spinner: false,
+            data:action.data,
+            uri:action.uri
+        };
+    },
+});
 
-const image = (state = initialState, action) => {
-  switch(action.type){
 
-    case 'ADD_IMAGE': 
-      return{
-        ...state,
-        imageIsAdded: true,
-        spinner: false
-      }
-    case 'SET_SPINNER':
-      return {
-        ...state,
-        spinner: true
-      }
-    case 'SET_SPINNER_TO_FALSE':
-      return {
-        ...state,
-        spinner: false
-      }
-    case 'GET_IMAGE': 
-      return {
-        ...state,
-        imageIsAdded: false,
-        imageCollection: action.payload
-      }
-    default: return state
-  }
-}
+// const imageReducer = (state = initialState, action) => {
+//   switch(action.type){
 
-export default image
+//     case 'ADD_IMAGE': 
+//       return{
+//         ...state,
+//         imageIsAdded: true,
+//         spinner: false,
+//         data:action.data,
+//         uri:action.uri
+//       }
+//     case 'SET_SPINNER':
+//       return {
+//         ...state,
+//         spinner: true
+//       }
+//     case 'SET_SPINNER_TO_FALSE':
+//       return {
+//         ...state,
+//         spinner: false
+//       }
+//     case 'GET_IMAGE': 
+//       return {
+//         ...state,
+//         imageIsAdded: false,
+//         imageCollection: action.payload
+//       }
+//     default: return state
+//   }
+// }
+
