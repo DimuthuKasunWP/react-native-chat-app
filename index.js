852576cc67e9f833a5adf3f22aa6975fa081d71c
Dimuthu <kasunwpdimuthu@gmail.com>
2020-07-12 22:54:56 +0530

    indexjs file changed

--- index.js
+++ index.js
@@ -2,52 +2,10 @@
  * @format
  */
 
-import  React, {useState} from 'react';
-import { AppRegistry,Animated } from 'react-native';
-import { Provider as PaperProvider } from 'react-native-paper';
-import { Provider } from 'react-redux';
+
+
+import {AppRegistry} from 'react-native';
 import App from './src/App';
-import { name as appName } from './app.json';
-import { theme } from './src/config/theme';
-import configureStore from 'src/store/configureStore';
-const { persistor, store } = configureStore();
-import NavigationStack from 'src/App';
-import { PersistGate } from 'redux-persist/es/integration/react';
-import { YellowBox } from 'react-native';
-import OfflineNotice from 'src/components/OfflineNotice'
+import {name as appName} from './app.json';
 
-YellowBox.ignoreWarnings([
-    'Warning: isMounted(...) is deprecated',
-    'Module RCTImageLoader',
-    'Warning: AsyncStorage '
-]);
-console.ignoreWarnings = [
-    'Warning: isMounted(...) is deprecated',
-    'Module RCTImageLoader',
-    'AsyncStorage '
-    ];
-    // console.disableYellowBox = true;
-
-export default function Main() {
-    const [animatePress, setAnimatePress] = useState(new Animated.Value(1))
-
-    const animateIn = () => {
-    Animated.timing(animatePress, {
-        toValue: 0.5,
-        duration: 500,
-        useNativeDriver: true 
-    }).start();
-    }
-  return (
-            <Provider store={store}>
-                <PersistGate persistor={persistor}>
-                    <PaperProvider theme={theme}>
-                        <OfflineNotice />
-                        <NavigationStack />
-                    </PaperProvider>
-                </PersistGate>
-            </Provider>
-  );
-}
-
-AppRegistry.registerComponent(appName, () => Main);
+AppRegistry.registerComponent(appName, () => App);
