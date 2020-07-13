3ed2afcc4722bff86e0b438ab341b36d1696db21
Dimuthu <kasunwpdimuthu@gmail.com>
2020-07-13 22:14:44 +0530

    new actions added

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,12 @@
 // creating action creators
 import * as loginActions from './loginActions';
 import * as cameraActions from './cameraActions';
+import * as chatRoomActions from './chatRoomActions';
 
 export const ActionCreators = Object.assign(
     {},
     loginActions,
-    cameraActions
+    cameraActions,
+    chatRoomActions
   
 );
