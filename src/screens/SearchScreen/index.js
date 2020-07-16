98df921aa3d3564cc67b2b1f8c4afa0110a401b0
Dimuthu <kasunwpdimuthu@gmail.com>
2020-07-16 20:09:44 +0530

    message list added to search screen

--- src/screens/SearchScreen/index.js
+++ src/screens/SearchScreen/index.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import styles from './styles';
+import MessageList from 'src/components/MessageList';
 
 export default class SearchScreen extends Component {
     render() {
         return (
-            <View style={styles.container}>
-                <Text> SearchScreen </Text>
+               <View style={styles.container}>
+          
+                  <MessageList navigation={this.props.navigation} />
             </View>
         );
     }
