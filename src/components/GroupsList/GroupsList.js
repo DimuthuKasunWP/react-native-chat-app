b05a6b6392485ed064128f020fa75e8871e6c4ba
Dimuthu <kasunwpdimuthu@gmail.com>
2020-07-18 20:53:49 +0530

    default data removed

--- src/components/GroupsList/GroupsList.js
+++ src/components/GroupsList/GroupsList.js
@@ -7,46 +7,7 @@ import { firebaseDB } from "src/firebase";
 import auth from "@react-native-firebase/auth";
 
 const data = [
-    {
-        name: 'React Native Community',
-        last_active: '15 days ago',
-        members: 'Vicky, Alex Jacob, Bob, William + 320'
-    },
-    {
-        name: 'Android Developers Forum',
-        last_active: '30 days ago',
-        members: 'Vicky, Alex, Bob, William + 256'
-    },
-    {
-        name: 'iOs Developers',
-        last_active: '30 days ago',
-        members: 'Tom Jacob, Alex Jacob,Thomas Paul + 400'
-    },
-    {
-        name: 'Buddies',
-        last_active: '10 days ago',
-        members: 'Vicky, Alex, Bob, William + 356'
-    },
-    {
-        name: 'Birthday Celebration',
-        last_active: '5 days ago',
-        members: 'Tom Alex, Jacob Samuel, Sam, +12'
-    },
-    {
-        name: 'College Buddies',
-        last_active: '24 days ago',
-        members: 'Vicky, Alex, Bob, William + 10'
-    },
-    {
-        name: 'Memories',
-        last_active: '1 day ago',
-        members: 'Tom Manuel, Jacob Augustin,Sam TOny +2'
-    },
-    {
-        name: 'Secret Group',
-        last_active: '28 days ago',
-        members: 'Tom Alex,Jacob Mathews,Sam Tony'
-    }
+    
 ];
 
 export default class GroupsList extends Component {
